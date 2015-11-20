84afe13aafe0c112a0e56e39eb519670720acf47
Luke <lukealization@gmail.com>
2015-11-20 16:41:24 +1300

    typehinting

--- src/Classes/PredicateQueries.php
+++ src/Classes/PredicateQueries.php
@@ -1,9 +1,10 @@
 <?php
 namespace LukeNZ\Ephemeris\Classes;
+use LukeNZ\Ephemeris\SatelliteIdentifiers\SatelliteIdentificationContract;
 
 class PredicateQueries
 {
-    public function satellite($satelliteIdentifiers) {
+    public function satellite(SatelliteIdentificationContract $satelliteIdentifiers) {
         if (is_array($satelliteIdentifiers)) {
 
         } else {
